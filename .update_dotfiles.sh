e62e38a8333003b967c6a28cecb445c5fafacf7a
Nick Gerace <nickagerace@gmail.com>
2019-06-05 02:16:25 -0400

    Add updating script for relevant dotfiles

---
+++ .update_dotfiles.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+##
+# Dotfiles
+# created by: Nick Gerace
+#
+# MIT License, Copyright (c) Nick Gerace
+# See "LICENSE" file for more information
+#
+# Please find license and further
+# information via the link below.
+# https://github.com/nickgerace/dotfiles
+##
+
+echo "[dotfiles] Copying files to repository..."
+cp ~/.zshrc ~/github/dotfiles
+cp ~/.vimrc ~/github/dotfiles
+cp ~/.aliases ~/github/dotfiles
+git -C ~/github/dotfiles status
+echo "[dotfiles] Update complete!"
