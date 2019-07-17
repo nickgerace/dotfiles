b5f863b71d8a5c56fa6dfd6ebefdc836da02504b
nickgerace <nickagerace@gmail.com>
2019-07-16 22:55:20 -0400

    Fix update dotfiles to include extra

--- .aliases/personal.bash
+++ .aliases/personal.bash
@@ -1,4 +1,4 @@
-alias update-dotfiles='cp ~/.zshrc ~/github/dotfiles && cp ~/.vimrc ~/github/dotfiles && cp -r ~/.aliases ~/github/dotfiles'
+alias update-dotfiles='cp ~/.zshrc ~/github/dotfiles && cp ~/.vimrc ~/github/dotfiles && cp -r ~/.aliases ~/github/dotfiles && cp -r ~/.extra ~/github/dotfiles'
 
 alias check-git-status='python3 ~/github/py-git-status/py_git_status.py ~/github'
 
---
+++ .aliases/update_dotfiles.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+##
+# UPDATE DOTFILES
+# created by: Nick Gerace
+#
+# MIT License, Copyright (c) Nick Gerace
+# See "LICENSE" file for more information
+#
+# Please find license and further
+# information via the link below.
+# https://github.com/nickgerace/dotfiles
+##
+
+cp ~/.zshrc ~/github/dotfiles
+cp ~/.vimrc ~/github/dotfiles
+cp -r ~/.aliases ~/github/dotfiles
