13e5759925f3b49d6805558d74d71197baae94a3
nickgerace <nickagerace@gmail.com>
2019-07-23 00:02:46 -0400

    Remove personal and add metro to internal

--- .aliases/internal.bash
+++ .aliases/internal.bash
@@ -1,5 +1,6 @@
 alias update-dotfiles='cp ~/.zshrc ~/git/dotfiles && cp ~/.vimrc ~/git/dotfiles && cp -r ~/.aliases ~/git/dotfiles && cp -r ~/.extra ~/git/dotfiles'
 
-alias check-git-status='python3 ~/git/py-git-status/lib/py_git_status.py ~/git'
+alias metro='python3 ~/git/metro/metro/cli.py'
+alias metro-git-status='metro status --git ~/git'
 
 alias vinternal='vim ~/.aliases/internal.bash'
--- .aliases/personal.bash
+++
@@ -1,5 +0,0 @@
-alias update-dotfiles='cp ~/.zshrc ~/github/dotfiles && cp ~/.vimrc ~/github/dotfiles && cp -r ~/.aliases ~/github/dotfiles && cp -r ~/.extra ~/github/dotfiles'
-
-alias check-git-status='python3 ~/github/py-git-status/py_git_status.py ~/github'
-
-alias vpersonal='vim ~/.aliases/personal.bash'
