ed429797970d5310954386ac102c472fcc0bf214
nickgerace <nickagerace@gmail.com>
2019-07-25 23:53:12 -0400

    Add zsh theme

--- .aliases/internal.bash
+++ .aliases/internal.bash
@@ -1,6 +1,6 @@
-alias update-dotfiles='cp ~/.zshrc ~/git/dotfiles && cp ~/.vimrc ~/git/dotfiles && cp -r ~/.aliases ~/git/dotfiles && cp -r ~/.extra ~/git/dotfiles'
+alias update-dotfiles='cp ~/.zshrc ~/git/dotfiles && cp ~/.vimrc ~/git/dotfiles && cp -r ~/.aliases ~/git/dotfiles && cp -r ~/.extra ~/git/dotfiles && cp ~/.oh-my-zsh/themes/nickgerace.zsh-theme ~/git/dotfiles/.oh-my-zsh/themes/'
 
 alias metro='python3 ~/git/metro/run_metro'
-alias metro-git-status='metro --git-status-path ~/git'
+alias metro-git-status='metro status -d ~/git'
 
 alias vinternal='vim ~/.aliases/internal.bash'
