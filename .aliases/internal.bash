5603a7d428abbf03a7891d442c26444486a3cd20
nickgerace <nickagerace@gmail.com>
2019-07-30 00:09:07 -0400

    Update metro path to app py

--- .aliases/internal.bash
+++ .aliases/internal.bash
@@ -1,6 +1,6 @@
 alias update-dotfiles='cp ~/.zshrc ~/git/dotfiles && cp ~/.vimrc ~/git/dotfiles && cp -r ~/.aliases ~/git/dotfiles && cp -r ~/.extra ~/git/dotfiles && cp ~/.oh-my-zsh/themes/nickgerace.zsh-theme ~/git/dotfiles/.oh-my-zsh/themes/'
 
-alias metro='python3 ~/git/metro/run_metro'
-alias metro-git-status='metro status -d ~/git'
+alias metro='python3 ~/git/metro/app.py'
+alias metro-git-status='metro status ~/git -r'
 
 alias vinternal='vim ~/.aliases/internal.bash'
