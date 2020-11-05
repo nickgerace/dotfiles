a299c25f3ca513e711104dee88af2121b768de6a
Nick Gerace <nick.gerace@rancher.com>
2020-11-05 12:52:52 -0500

    Add Rancher install script
    Add Rancher install script. Add helm install to kubectl script. Remove
    root options.

--- .scripts/docker-ubuntu-install.sh
+++ .scripts/docker-ubuntu-install.sh
@@ -1,35 +1,17 @@
 #!/usr/bin/env bash
-if [ "$EUID" -ne 0 ]; then
-    sudo apt update
-    sudo apt-get install -y \
-        apt-transport-https \
-        ca-certificates \
-        curl \
-        gnupg-agent \
-        software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo apt-key fingerprint 0EBFCD88
-    sudo add-apt-repository \
-        "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-        $(lsb_release -cs) \
-        stable"
-    sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-    sudo usermod -aG docker $(whoami)
-else
-    apt update
-    apt-get install -y \
-        apt-transport-https \
-        ca-certificates \
-        curl \
-        gnupg-agent \
-        software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-    apt-key fingerprint 0EBFCD88
-    add-apt-repository \
-        "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-        $(lsb_release -cs) \
-        stable"
-    apt-get update
-    apt-get install docker-ce docker-ce-cli containerd.io -y
-fi
+sudo apt update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo usermod -aG docker $(whoami)
---
+++ .scripts/rancher.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+if [ ! $1 ]; then
+    printf "Requires argument(s): <subcommand>\n"
+    exit 1
+fi
+
+if [ ! -f $HOME/.kube/config ]; then
+    printf "File not found: ${HOME}/.kube/config\nTrying k3s..\n"
+    export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+fi
+
+if [ "$1" == "install" ]; then
+    if [ ! $2 ]; then
+        printf "Requires argument(s): <rancher-ui-hostname>\n"
+        exit 1
+    fi
+
+    kubectl create --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.15.0/cert-manager.crds.yaml
+    kubectl create namespace cert-manager
+    helm repo add jetstack https://charts.jetstack.io
+    helm repo update
+    helm install cert-manager jetstack/cert-manager -n cert-manager --version v0.15.0
+    kubectl rollout status deployment cert-manager -n cert-manager
+    kubectl rollout status deployment cert-manager-webhook -n cert-manager
+    kubectl rollout status deployment cert-manager-cainjector -n cert-manager
+
+    helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
+    kubectl create namespace cattle-system
+    helm install rancher rancher-latest/rancher -n cattle-system --set hostname=${2}
+    kubectl rollout status deploy/rancher -n cattle-system
+elif [ "$1" == "delete" ] || [ "$1" == "uninstall" ]; then
+    helm uninstall -n rancher rancher
+    kubectl delete namespace cattle-system
+
+    helm uninstall -n cert-manager cert-manager
+    kubectl delete -f https://github.com/jetstack/cert-manager/releases/download/v0.15.0/cert-manager.crds.yaml
+    kubectl delete namespace cert-manager
+else
+    printf "Possible subcommands: install delete/uninstall\n"
+    exit 1
+fi
