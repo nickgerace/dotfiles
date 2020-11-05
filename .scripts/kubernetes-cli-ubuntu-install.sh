60d487f001bf78344d8d9c117b1e25789cf3d6b9
Nick Gerace <nick.gerace@rancher.com>
2020-11-05 12:52:52 -0500

    Add Rancher install script
    Add Rancher install script. Add helm install to kubectl script. Remove
    root options.

---
+++ .scripts/kubernetes-cli-ubuntu-install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+printf "[+] Installing kubectl ...\n"
+sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2 curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+printf "[+] Installing helm ...\n"
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+sudo apt-get install apt-transport-https --yes
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
+
+printf "[+] Done!\n"
