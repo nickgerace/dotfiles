7c04d224e1484605130ecbb89400b69f254c85b4
Nick Gerace <nickagerace@gmail.com>
2020-11-10 01:33:08 -0500

    Consolidate macOS Makefile

--- mk/mac.mk
+++ mk/mac.mk
@@ -1,34 +1,20 @@
 # DOTFILES
 # https://nickgerace.dev
 
-brew: brew-update-and-upgrade
-	-brew install \
-		bash curl git make tmux wget bash-completion@2 \
-		aspell htop speedtest-cli gsed jq tree neovim \
-		go helm k3d kind kubectl rke kustomize k9s
-	-brew install \
-		golangci/tap/golangci-lint
-	-brew tap \
+brew-mac-install: brew-mac-update-and-upgrade
+	brew install \
+		bash curl git make tmux wget bash-completion@2 neovim \
+		aspell htop speedtest-cli gsed jq tree nodejs \
+		cowsay lolcat fortune neofetch \
+		doctl azure-cli \
+		go helm k3d kind kubectl rke kustomize k9s golangci/tap/golangci-lint
+	brew tap \
 		homebrew/cask-fonts
-	-brew cask install \
+	brew cask install \
 		font-iosevka font-iosevka-slab font-cascadia-code \
-		ngrok
+		ngrok google-cloud-sdk
 
-brew-update-and-upgrade:
+brew-mac-update-and-upgrade:
 	brew update
 	brew upgrade
-
-brew-memes:
-	brew install \
-		cowsay lolcat fortune neofetch
-
-brew-cloud:
-	brew install \
-		doctl azure-cli
-	brew cask install \
-		google-cloud-sdk
-
-brew-extras:
-	brew install \
-		nodejs
 
