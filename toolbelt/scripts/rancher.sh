7e741b6e8da3aadd39820579fda6cb125669f607
Nick Gerace <nick.gerace@rancher.com>
2020-11-11 18:58:46 -0500

    Add image tags to Rancher script

--- toolbelt/scripts/rancher.sh
+++ toolbelt/scripts/rancher.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ ! $1 ]; then
-    printf "Requires argument(s): <subcommand>\n"
+    printf "Argument(s): <subcommand>\n"
     exit 1
 fi
 
@@ -9,33 +9,57 @@ if [ ! -f $HOME/.kube/config ]; then
     export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 fi
 
+CM_VERSION=v0.15.0
+NS_CM=cert-manager
+NS_RANCHER=cattle-system
+
 if [ "$1" == "create" ] || [ "$1" == "install" ]; then
     if [ ! $2 ]; then
-        printf "Requires argument(s): <rancher-ui-hostname>\n"
+        printf "Argument(s): <rancher-ui-hostname> <optional-rancher-image-tag>\n"
         exit 1
     fi
 
-    kubectl create --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.15.0/cert-manager.crds.yaml
-    kubectl create namespace cert-manager
+    if [ $3 ]; then
+        printf "Rancher image tag provided: ${3}\n"
+        docker pull rancher/rancher:${3}
+        if [ $? -ne 0 ]; then
+            printf "Invalid image provided. Valid images: https://hub.docker.com/r/rancher/rancher/tags/\n"
+            exit 1
+        fi
+    fi
+
+    kubectl create --validate=false -f https://github.com/jetstack/cert-manager/releases/download/${CM_VERSION}/cert-manager.crds.yaml
+    kubectl create namespace ${NS_CM}
     helm repo add jetstack https://charts.jetstack.io
     helm repo update
-    helm install cert-manager jetstack/cert-manager -n cert-manager --version v0.15.0
-    kubectl rollout status deployment cert-manager -n cert-manager
-    kubectl rollout status deployment cert-manager-webhook -n cert-manager
-    kubectl rollout status deployment cert-manager-cainjector -n cert-manager
+    helm install cert-manager jetstack/cert-manager -n ${NS_CM} --version ${CM_VERSION}
+    kubectl rollout status deployment cert-manager -n ${NS_CM}
+    kubectl rollout status deployment cert-manager-webhook -n ${NS_CM}
+    kubectl rollout status deployment cert-manager-cainjector -n ${NS_CM}
 
     helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
-    kubectl create namespace cattle-system
-    helm install rancher rancher-latest/rancher -n cattle-system --set hostname=${2}
-    kubectl rollout status deploy/rancher -n cattle-system
+    kubectl create namespace ${NS_RANCHER}
+    if [ $3 ]; then
+        helm install rancher rancher-latest/rancher -n ${NS_RANCHER} \
+            --set hostname=${2} \
+            --set rancherImageTag=${3} \
+            --set rancherImagePullPolicy=Always \
+            --set replicas=1
+    else
+        helm install rancher rancher-latest/rancher -n ${NS_RANCHER} \
+            --set hostname=${2} \
+            --set rancherImagePullPolicy=Always \
+            --set replicas=1
+    fi
+    kubectl rollout status deploy/rancher -n ${NS_RANCHER}
 elif [ "$1" == "delete" ] || [ "$1" == "uninstall" ]; then
-    helm uninstall -n cattle-system rancher
-    kubectl delete namespace cattle-system
+    helm uninstall -n ${NS_RANCHER} rancher
+    kubectl delete namespace ${NS_RANCHER}
 
-    helm uninstall -n cert-manager cert-manager
-    kubectl delete -f https://github.com/jetstack/cert-manager/releases/download/v0.15.0/cert-manager.crds.yaml
-    kubectl delete namespace cert-manager
+    helm uninstall -n ${NS_CM} cert-manager
+    kubectl delete -f https://github.com/jetstack/cert-manager/releases/download/${CM_VERSION}/cert-manager.crds.yaml
+    kubectl delete namespace ${NS_CM}
 else
-    printf "Possible subcommands: create/install delete/uninstall\n"
+    printf "Subcommand(s): create/install delete/uninstall\n"
     exit 1
 fi
