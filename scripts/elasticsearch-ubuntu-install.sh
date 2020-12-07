52d8c2708a0b0fec6c10771b9225826e74f59b25
Nick Gerace <nick.gerace@rancher.com>
2020-12-07 16:38:34 -0500

    Add elasticsearch script

---
+++ scripts/elasticsearch-ubuntu-install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -    
+sudo apt-get install -y apt-transport-https    
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-7.x.list    
+sudo apt-get update    
+sudo apt-get install -y elasticsearch
