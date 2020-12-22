8caf6c53347548a0e4e1d461d89d5fcc2fa5a2ff
Nick Gerace <nick.gerace@rancher.com>
2020-12-22 11:31:22 -0500

    Make kibana optional

--- scripts/elasticsearch-ubuntu-install.sh
+++ scripts/elasticsearch-ubuntu-install.sh
@@ -9,9 +9,12 @@ sudo /bin/systemctl enable elasticsearch.service
 sudo systemctl start elasticsearch.service
 sleep 10
 curl -X GET "localhost:9200/?pretty"
-sudo apt-get install -y kibana
-sudo /bin/systemctl daemon-reload
-sudo /bin/systemctl enable kibana.service
-sudo systemctl start kibana.service
-sleep 10
-printf "ElasticSearch and Kibana should be running!\n"
+
+if [ $1 ] && [ "${1}" == "kibana"]; then
+    sudo apt-get install -y kibana
+    sudo /bin/systemctl daemon-reload
+    sudo /bin/systemctl enable kibana.service
+    sudo systemctl start kibana.service
+    sleep 10
+    printf "ElasticSearch and Kibana should be running!\n"
+fi
