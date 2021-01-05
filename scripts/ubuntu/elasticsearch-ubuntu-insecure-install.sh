ed21ed3249e6870d1c6bf71181be858222ad0194
Nick Gerace <nick.gerace@rancher.com>
2021-01-05 14:16:38 -0500

    Fix elasticsearch install

--- scripts/ubuntu/elasticsearch-ubuntu-insecure-install.sh
+++ scripts/ubuntu/elasticsearch-ubuntu-insecure-install.sh
@@ -4,17 +4,15 @@ sudo apt-get install -y apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-7.x.list    
 sudo apt-get update    
 sudo apt-get install -y elasticsearch
-sudo /bin/systemctl daemon-reload
-sudo /bin/systemctl enable elasticsearch.service
+sudo apt autoremove -y
+echo "network.host: 0.0.0.0" >> /etc/elasticsearch/elasticsearch.yml
+echo "http.port: 9200" >> /etc/elasticsearch/elasticsearch.yml
+echo "discovery.type: single-node" >> /etc/elasticsearch/elasticsearch.yml
+sudo ufw allow 9200
+sudo ufw enable
+sudo systemctl daemon-reload
+sudo systemctl enable elasticsearch.service
 sudo systemctl start elasticsearch.service
 sleep 10
 curl -X GET "localhost:9200/?pretty"
-
-if [ $1 ] && [ "${1}" == "kibana"]; then
-    sudo apt-get install -y kibana
-    sudo /bin/systemctl daemon-reload
-    sudo /bin/systemctl enable kibana.service
-    sudo systemctl start kibana.service
-    sleep 10
-    printf "ElasticSearch and Kibana should be running!\n"
-fi
+printf "[WARNING] This is an insecure ElasticSearch setup meant for development only.\n"
