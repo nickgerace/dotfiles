2a927bc2e7795464578b31d7b4999f3ab84ae749
Nick Gerace <nick.gerace@rancher.com>
2021-01-08 16:36:49 -0500

    Add version to k3s install

--- scripts/k3s-install.sh
+++ scripts/k3s-install.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 if [ ! "$(command -v kubectl)" ] || [ ! "$(command -v docker)" ] || [ ! "$(command -v helm)" ]; then
-    printf "Recommended dependencies: kubectl docker helm\nSleeping 10 seconds for CTRL+C...\n"
-    sleep 10
+    printf "Recommended dependencies: kubectl docker helm\n"
+    exit 1
 fi
-curl -sfL https://get.k3s.io | sh -
+if [ $1 ]; then
+    curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=${1} sh -
+else
+    curl -sfL https://get.k3s.io | sh -
+fi
 printf "\nConfig file location: /etc/rancher/k3s/k3s.yaml\n"
