a03aa0087ae428e30bfda4df28bd4bd1b4b5e86e
Nick Gerace <nickagerace@gmail.com>
2021-01-10 23:40:15 -0500

    Add home-manager aliases

--- zsh/nix.zsh
+++ zsh/nix.zsh
@@ -5,4 +5,7 @@ if [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then
     export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
     source $HOME/.nix-profile/etc/profile.d/nix.sh
     alias hm="home-manager"
+    alias hme="home-manager edit"
+    alias hms="home-manager switch"
+    alias hml="home-manager packages | sort"
 fi
