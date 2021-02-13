86a4a8fb50958ee4fc13c895f6caf96bfcc73f15
Nick Gerace <nickagerace@gmail.com>
2021-02-12 22:39:07 -0500

    Add kubectl to dnf packages

--- zsh/cloud.zsh
+++ zsh/cloud.zsh
@@ -1,1 +1,10 @@
 alias aws-login="aws configure"
+
+function aws-create-key-pair {
+    if [ ! $1 ] || [ ! $2 ]; then
+        printf "Required argument(s): <key-name> <path-to-directory>\n"
+    else
+        ( cd ${2}; aws ec2 create-key-pair --key-name ${1} --query "KeyMaterial" --output text > ${1}.pem )
+        ( cd ${2}; chmod 400 ${1}.pem )
+    fi
+}
--- zsh/rancher.zsh
+++ zsh/rancher.zsh
@@ -2,15 +2,21 @@ if [ -d "/usr/local/opt/yq@3/bin" ]; then
     export PATH="/usr/local/opt/yq@3/bin:$PATH"
 fi
 
-function rc {
+function rc-logging {
     if [ ! $1 ]; then
-        printf "Required argument(s): <command> <optional-package-name>\nExample commands: prepare patch clean charts\n"
+        printf "Required argument(s): <command>\nExample commands: prepare patch clean charts\n"
     else
-        TEMP_PACKAGE=rancher-logging 
-        if [ $2 ]; then
-            TEMP_PACKAGE="${2}"
-        fi
-        ( cd $HOME/rancher-charts; PACKAGE=${TEMP_PACKAGE} make ${1} )
+        ( cd $HOME/rancher-charts; PACKAGE=rancher-logging make ${1} )
+    fi
+}
+
+function rc-fleet {
+    if [ ! $1 ]; then
+        printf "Required argument(s): <command>\nExample commands: prepare patch clean charts\n"
+    else
+        ( cd $HOME/rancher-charts; PACKAGE=fleet make ${1} )
+        ( cd $HOME/rancher-charts; PACKAGE=fleet-crd make ${1} )
+        ( cd $HOME/rancher-charts; PACKAGE=fleet-agent make ${1} )
     fi
 }
 
