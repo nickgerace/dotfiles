f78e5b5343625dfd000c9db3d0450f711dde2b10
Nick <nickagerace@gmail.com>
2021-03-18 22:15:42 -0400

    Remove make and sed aliases for macOS

--- zsh/darwin.zsh
+++ zsh/darwin.zsh
@@ -2,8 +2,6 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     export ZSH_DISABLE_COMPFIX=true
     alias fix-compaudit-errors-on-macos="compaudit | xargs chmod g-w"
     alias ls="ls -G"
-    if [ "$(command -v gsed)" ]; then alias sed="gsed"; fi
-    if [ "$(command -v gmake)" ]; then alias make="gmake"; fi
 else
     alias ls="ls --color=auto"
 fi
