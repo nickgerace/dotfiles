8987c6d351abe64eef96f15aee635a50d1790d58
Nick Gerace <nick.gerace@suse.com>
2021-04-21 18:20:49 -0400

    Fix rancher generate images script

--- scripts/rancher-generate-images.sh
+++ scripts/rancher-generate-images.sh
@@ -3,15 +3,14 @@
 set -ex
 
 # Change these values to your needs and liking.
+RANCHER=$HOME/github.com/rancher/rancher
 RANCHER_SYSTEM_CHARTS=$HOME/github.com/rancher/system-charts
-RANCHER_CHARTS=$HOME/github.com/rancher/charts
+RANCHER_CHARTS=$HOME/github.com/nickgerace/rancher-charts
 RANCHER_METADATA_BRANCH=dev-v2.5
 RANCHER_SYSTEM_CHARTS_BRANCH=dev-v2.5
 RANCHER_CHARTS_BRANCH=dev-v2.5
 SERVER_TAG=v2.5-head
 AGENT_TAG=v2.5-head
-
-cd $(dirname "$0")
 
 function check-branch {
     if [ "$(cd $1; git branch --show-current)" != "$2" ]; then
@@ -20,11 +19,16 @@ function check-branch {
     fi
 }
 
+# Add the "-i" flag before "rm" if you'd like to protect against accidental deletion.
 [ ! -d $HOME/bin ] && mkdir -p $HOME/bin
-[ -f $HOME/bin/data.json ] && rm -i $HOME/bin/data.json
+[ -f $HOME/bin/data.json ] && rm $HOME/bin/data.json
 
 check-branch $RANCHER_SYSTEM_CHARTS $RANCHER_SYSTEM_CHARTS_BRANCH
 check-branch $RANCHER_CHARTS $RANCHER_CHARTS_BRANCH
 
 curl -sLf https://releases.rancher.com/kontainer-driver-metadata/${RANCHER_METADATA_BRANCH}/data.json > $HOME/bin/data.json
-REPO=rancher TAG=dev go run $(dirname "$0"; pwd)/pkg/image/export/main.go $RANCHER_SYSTEM_CHARTS $RANCHER_CHARTS rancher/rancher:$SERVER_TAG rancher/rancher-agent:$AGENT_TAG
+
+cd $RANCHER
+REPO=rancher TAG=dev go run $RANCHER/pkg/image/export/main.go \
+    $RANCHER_SYSTEM_CHARTS $RANCHER_CHARTS \
+    rancher/rancher:$SERVER_TAG rancher/rancher-agent:$AGENT_TAG
