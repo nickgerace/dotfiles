c25c702744eb562f0bc555bdc2f808bdb419be89
Nick Gerace <nickagerace@gmail.com>
2021-08-10 21:06:59 -0400

    Remove rebase footgun

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 MAKEPATH:=$(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
 
-all: prepare neovim-theme
+all: prepare neovim-theme git-config
 	-rm $(HOME)/.zshrc
 	ln -s $(MAKEPATH)/zshrc $(HOME)/.zshrc
 	-rm $(HOME)/.tmux.conf
@@ -15,3 +15,6 @@ neovim-theme:
 	-rm $(HOME)/.config/nvim/colors/one.vim
 	curl https://raw.githubusercontent.com/rakr/vim-one/master/colors/one.vim \
 		-o $(HOME)/.config/nvim/colors/one.vim
+
+git-config:
+	git config --global pull.rebase true
--- zsh/git.zsh
+++ zsh/git.zsh
@@ -48,17 +48,16 @@ function post-merge {
 }
 
 function rebase-forked-repo {
-    if [ ! $1 ] || [ ! $2 ]; then
-        echo "requires arguments: <original-branch> <forked-branch> <OPTIONAL-provide-full-remote-address>"
+    if [ ! $1 ]; then
+        echo "requires arguments: <upstream-branch> <OPTIONAL-provide-full-remote-address>"
         return
     fi
-    if [ $3 ] && [ "$3" != "" ]; then
-        git remote add upstream $3
+    if [ $2 ] && [ "$2" != "" ]; then
+        git remote add upstream $2
         git remote -v
     fi
     git fetch upstream
     git rebase upstream/$1
-    git push origin $2 --force
 }
 
 function git-checkout-tag {
