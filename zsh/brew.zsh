18893c925fa88f7a6b3553c6b1b44e36b3a2d243
Nick Gerace <nickagerace@gmail.com>
2021-11-02 12:19:27 -0400

    Fix older homebrew installs

--- zsh/brew.zsh
+++ zsh/brew.zsh
@@ -1,4 +1,8 @@
 if [ "$NICK_OS" = "darwin" ]; then
-    eval "$(/opt/homebrew/bin/brew shellenv)"
-    export PATH="/opt/homebrew/opt/ruby/bin:$PATH"
+    if [ -d /opt/homebrew/bin ]; then
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+    fi
+    if [ -d /opt/homebrew/opt/ruby/bin ]; then
+        export PATH="/opt/homebrew/opt/ruby/bin:$PATH"
+    fi
 fi
