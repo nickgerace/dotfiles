dc911a185e62e95609c6202df97567dd138392a9
Nick Gerace <nick.gerace@suse.com>
2021-11-08 12:49:12 -0500

    Fix Rancher single node upgrade

--- scripts/rancher-docker-upgrade.sh
+++ scripts/rancher-docker-upgrade.sh
@@ -1,14 +1,26 @@
 #!/usr/bin/env bash
 set -e
-if [ ! $1 ] || [ ! $2 ]; then    
-    echo "required argument: <rancher-container-name> <new-tag> <optional-volume-name>"    
+
+if [ ! $1 ] || [ ! $2 ]; then
+    echo "-----------------------------------------------------------------------------------------------------------"
+    echo "required arguments  |  <rancher-container-name>  <new-tag>         <optional-new-image-name>"
+    echo "descriptions        |  Docker container ID       Docker image tag  image name (defaults to rancher/rancher)"
+    echo "example #1          |  f7ae457bd1ff              v2.6.2            nickgerace/rancher"
+    echo "example #2          |  f576fbbd7b15              latest"
+    echo "-----------------------------------------------------------------------------------------------------------"
     exit 1
-fi    
-local VOLUME_NAME=rancher-data    
-if [ $3 ] && [ "$3" != "" ]; then    
-    VOLUME_NAME=$3    
-fi    
-local OLD_TAG=$(docker container inspect $1 | jq ".[0].Config.Image" | tr -d '"' | cut -d ":" -f2)    
-docker stop $1                                                                  
-docker create --volumes-from $1 --name rancher-data rancher/rancher:$OLD_TAG                                              
-docker run -d --privileged --volumes-from $VOLUME_NAME --restart=unless-stopped -p 80:80 -p 443:443 rancher/rancher:$2
+fi
+
+IMAGE_NAME=rancher/rancher
+if [ $3 ] && [ "$3" != "" ]; then
+    IMAGE_NAME=$3
+fi
+
+OLD_IMAGE=$(docker container inspect $1 | jq ".[0].Config.Image" | tr -d '"')
+docker stop $1
+docker create --volumes-from $1 --name rancher-data $OLD_IMAGE
+docker run -d --privileged \
+    --volumes-from rancher-data
+    --restart=unless-stopped -p 80:80 -p 443:443 \
+    --no-cacerts
+    $IMAGE_NAME:$2
