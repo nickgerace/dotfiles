d801ea2c491b4a6ba39f9beab6a4d961c12fd2c3
Nick Gerace <nickagerace@gmail.com>
2022-01-03 15:22:29 -0500

    Remove gfld function
    - Remove gfld function in favor of gfold 3.0.0 RCs
    
    Signed-off-by: Nick Gerace <nickagerace@gmail.com>

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -127,23 +127,3 @@ function gpob {
     git push origin $(git branch --show-current)
 }
 
-function gfld {
-    if ! [ "$(command -v fd)" ]; then
-        echo "fd needs to be installed in an PATH"
-        return
-    fi
-
-    local FIRST="true"
-    for i in $(fd -t d); do
-        if [ -d $i/.git ]; then
-            if [ "$FIRST" = "true" ]; then
-                FIRST="false"
-            else
-                echo ""
-            fi
-
-            echo "📡 $i"
-            ( cd $i; git status -s; git config --get remote.origin.url; git config --get user.email )
-        fi
-    done
-}
