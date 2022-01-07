201206745343602674c18722a9c1f2fa6ee62c71
Nick Gerace <nickagerace@gmail.com>
2022-01-07 18:25:02 -0500

    Add tmux enter
    Signed-off-by: Nick Gerace <nickagerace@gmail.com>

--- zsh/tmux.zsh
+++ zsh/tmux.zsh
@@ -2,6 +2,7 @@ if [ "$(command -v tmux)" ]; then
     alias tmuxa="tmux attach -t"
     alias tmuxk="tmux kill-session -t"
     alias tmuxn="tmux new -s"
+    alias tmuxe="cd ~/src && tmux"
 
     alias tmuxl="tmux ls"
     alias tl="tmux ls"
