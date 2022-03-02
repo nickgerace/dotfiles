c8b34999f5f0752c639436801c6ac6cf85be9856
Nick Gerace <nickagerace@gmail.com>
2022-03-02 16:37:55 -0500

    Add cargo watch
    Signed-off-by: Nick Gerace <nickagerace@gmail.com>

--- zsh/rust.zsh
+++ zsh/rust.zsh
@@ -8,6 +8,7 @@ if [ "$(command -v cargo)" ]; then
     alias ct="cargo test"
     alias cx="cargo xtask"
 
+    alias cargo-doc-watch="cargo watch -s 'cargo doc --all'"
     alias cargo-wipe="$NICK_DOTFILES/scripts/cargo-wipe.sh $NICK_SRC"
 fi
 
