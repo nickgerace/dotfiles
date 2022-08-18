2c922651842c9bb9fbe65b0c42c78402cfd06994
Nick Gerace <nickagerace@gmail.com>
2022-08-18 14:30:02 -0400

    Use mold for rust on darwin
    Signed-off-by: Nick Gerace <nickagerace@gmail.com>

--- zsh/darwin.zsh
+++ zsh/darwin.zsh
@@ -2,6 +2,11 @@ if [ "$NICK_OS" = "darwin" ]; then
     export ZSH_DISABLE_COMPFIX=true
     alias fix-compaudit-errors-on-macos="compaudit | xargs chmod g-w"
     alias ls="ls -G"
+
+    # Use GNU Make instead of BSD Make.
+    if [ -d /opt/homebrew/opt/make/libexec/gnubin ]; then
+        alias PATH="/opt/homebrew/opt/make/libexec/gnubin:$PATH"
+    fi
 else
     alias ls="ls --color=auto"
 fi
