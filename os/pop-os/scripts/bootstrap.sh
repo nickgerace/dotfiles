73619b441b0bad230bb165b520250e9fe76b50e5
Nick Gerace <nickagerace@gmail.com>
2022-10-12 20:25:46 -0400

    Update Pop!_OS stuff for 22.04 LTS
    Signed-off-by: Nick Gerace <nickagerace@gmail.com>

--- os/pop-os/scripts/bootstrap.sh
+++ os/pop-os/scripts/bootstrap.sh
@@ -3,6 +3,8 @@ set -e
 
 TRUE_USER="$(whoami)"
 DOTFILES="$HOME/src/dotfiles"
+BASE="$DOTFILES/os/pop-os/scripts/base.txt"
+CRATES="$DOTFILES/crates.txt"
 
 function die {
   if [ "$1" = "" ]; then
@@ -22,7 +24,6 @@ function verify-config {
   if [ "$(grep '^ID=' /etc/os-release | sed 's/^ID=//' | tr -d '"')" != "pop" ]; then
     die "Linux distro must be Pop!_OS"
   fi
-  # shellcheck disable=SC2143
   if [ -f /proc/sys/kernel/osrelease ] && [ "$(grep "WSL2" /proc/sys/kernel/osrelease)" ]; then
     die "cannot use WSL2"
   fi
@@ -38,7 +39,7 @@ function verify-config {
 function prepare {
   sudo apt update
   sudo apt upgrade -y
-  xargs sudo apt install -y <"$DOTFILES"/scripts/pop-os/base.txt
+  xargs sudo apt install -y < $BASE
 }
 
 function final {
@@ -47,36 +48,38 @@ function final {
   sudo apt autoremove -y
 }
 
-function install-volta {
-  curl https://get.volta.sh | bash -s -- --skip-setup
-  volta install node
+function install-nodejs {
+  curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+  sudo apt update
+  sudo apt install -y nodejs
 }
 
 function install-rust {
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y
+  source "$HOME/.cargo/env"
   rustup toolchain install stable-x86_64-unknown-linux-gnu
   rustup toolchain install nightly-x86_64-unknown-linux-gnu
   rustup default stable-x86_64-unknown-linux-gnu
-  xargs cargo install --locked <"$DOTFILES"/crates.txt
+  xargs cargo install --locked < $CRATES
 }
 
 function install-docker {
   sudo apt update
-  sudo apt-get install -y ca-certificates curl gnupg lsb-release
+  sudo apt install -y ca-certificates curl gnupg lsb-release
+  sudo mkdir -p /etc/apt/keyrings
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   sudo apt update
-  sudo apt install -y docker-ce docker-ce-cli containerd.io
+  sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+  sudo service docker start
   sudo docker run hello-world
-  sudo usermod -aG docker "$TRUE_USER"
-  sudo systemctl enable docker.service
-  sudo systemctl enable containerd.service
+  sudo usermod -aG docker $TRUE_USER
 }
 
 verify-config
 prepare
-install-volta
+install-nodejs
 install-rust
 install-docker
 final
