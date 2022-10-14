a533f4bd3872bd15cdc42b34a82666b16d88dcdf
Nick Gerace <nickagerace@gmail.com>
2022-10-14 17:53:28 -0400

    Add rust automation and use linuxbrew

--- zsh/brew.zsh
+++ zsh/brew.zsh
@@ -5,4 +5,8 @@ if [ "$NICK_OS" = "darwin" ]; then
     if [ -d /opt/homebrew/opt/ruby/bin ]; then
         export PATH="/opt/homebrew/opt/ruby/bin:$PATH"
     fi
+elif [ "$NICK_LINUX" = "true" ]; then
+    if [ -d /home/linuxbrew/.linuxbrew/bin ]; then
+        eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    fi
 fi
