d953680d7ed2186778bd48416c257f184046d3d2
Nick Gerace <nickagerace@gmail.com>
2023-05-26 18:11:17 -0400

    Use volta instead of node and pnpm
    Signed-off-by: Nick Gerace <nickagerace@gmail.com>

--- zsh/node.zsh
+++ zsh/node.zsh
@@ -8,7 +8,3 @@ if [ -d "$HOME/Library/pnpm" ]; then
 
     alias pnpm-use-node="pnpm env use --global lts"
 fi
-
-if [ -d /opt/homebrew/opt/node@18/bin ]; then
-    PATH=/opt/homebrew/opt/node@18/bin:$PATH
-fi
