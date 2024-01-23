9b668ed8467116893163f1eca4de8b28c0712477
Nick Gerace <nickagerace@gmail.com>
2024-01-22 23:06:27 -0500

    Add increase inotify watches limit script for intellij-based IDEs
    Signed-off-by: Nick Gerace <nickagerace@gmail.com>

---
+++ scripts/rust-rover-increase-inotify-watches-limit.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+if [ $EUID -eq 0 ]; then
+    echo "must run as non-root"
+    exit 1
+fi
+
+sudo -v
+sudo echo "fs.inotify.max_user_watches = 524288" > /etc/sysctl.d/idea.conf
+sudo sysctl -p --system
