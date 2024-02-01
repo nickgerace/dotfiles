8dee20435e5e0c18a86862557e02a989626776a2
Nick Gerace <nickagerace@gmail.com>
2024-01-31 19:37:15 -0500

    Restore home-manager for Pop!_OS
    - Restore home-manager (for what feels like the millionth time) for
      Pop!_OS without a Nix Flake (i.e. just home-manager installed via
      Nix directly)
    - Refactor how linking and dotfiles setup works and centralize it in the
      bootstrap script
    - Only configure rustup when working with packages in the bootstrap
      script
    - Ensure home-manager works with the update script
    - Don't use "nix profile" commands in zsh dotfiles when home-manager is
      installed
    
    Signed-off-by: Nick Gerace <nickagerace@gmail.com>

--- zsh/nix.zsh
+++ zsh/nix.zsh
@@ -2,9 +2,20 @@ if [ -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]; then
     . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 fi
 
-if [ $(command -v nix) ]; then
+if [ "$(command -v nix)" ]; then
     alias nix-search="nix search nixpkgs"
     alias ndc="nix develop --command"
+fi
+
+if [ "$(command -v home-manager)" ]; then
+    . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+
+    alias hm="home-manager"
+    alias hme="home-manager edit"
+    alias hms="home-manager switch"
+    alias hml="home-manager packages | sort"
+else
+    # Only use nix profile commands is home-manager is not installed.
     alias nix-upgrade="nix profile upgrade '.*'"
 
     function nix-install {
@@ -16,7 +27,7 @@ if [ $(command -v nix) ]; then
     }
 fi
 
-if [ $(command -v direnv) ]; then
+if [ "$(command -v direnv)" ]; then
     eval "$(direnv hook zsh)"
 fi
 
