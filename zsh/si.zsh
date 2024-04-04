7eff80c953d507628afdcd6e79234664f08c7044
Nick Gerace <nickagerace@gmail.com>
2024-04-04 17:45:52 -0400

    Add si-branches function
    Signed-off-by: Nick Gerace <nickagerace@gmail.com>

--- zsh/si.zsh
+++ zsh/si.zsh
@@ -3,3 +3,23 @@ function start-work {
     cd ~/src/si
     tmux
 }
+
+function si-branches {
+    echo ""
+
+    for branch in $(git branch); do
+        if [[ $branch != *"*"* ]] && [[ $branch != "main" ]]; then
+            echo "  $branch    $(git log -1 --pretty=format:%s $branch)"
+        fi
+    done
+
+    echo ""
+
+    for branch in $(git branch -a); do
+        if [[ $branch == *"origin"* ]] && [[ $branch == *"nick"* ]]; then
+            echo "  $branch    $(git log -1 --pretty=format:%s $branch)"
+        fi
+    done
+
+    echo ""
+}
