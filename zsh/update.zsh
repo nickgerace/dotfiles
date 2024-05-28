927c576a6cf910d0ad1ba8e2c8795aa71b5dcbb3
Nick Gerace <nickagerace@gmail.com>
2024-05-28 12:32:39 -0400

    Fix permissions in update
    Signed-off-by: Nick Gerace <nickagerace@gmail.com>

--- zsh/update.zsh
+++ zsh/update.zsh
@@ -1,66 +1,84 @@
 function update {
+    function prepare-permissions {
+        sudo -v
+    }
+
+    function update-os-packages {
+        if [ "$NICK_OS" = "ubuntu" ] || [ "$NICK_OS" = "pop" ]; then
+            sudo apt update
+            sudo apt upgrade -y
+            sudo apt autoremove -y
+        elif [ "$NICK_OS" = "fedora" ]; then
+            if command -v dnf5; then
+                sudo dnf5 upgrade -y --refresh
+                sudo dnf autoremove -y
+            else
+                sudo dnf upgrade -y --refresh
+                sudo dnf autoremove -y
+            fi
+        elif [ "$NICK_OS" = "opensuse-tumbleweed" ]; then
+            sudo zypper update -y
+        elif [ "$NICK_OS" = "darwin" ] && command -v brew; then
+            brew update
+            brew upgrade
+            brew cleanup
+        fi
+    }
+
+    function update-rustup {
+        if command -v rustup; then
+            rustup update
+        fi
+    }
+
+    function update-neovim-plugins {
+        if [ -f "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim ] && command -v nvim; then
+            nvim --headless +PlugUpgrade +PlugUpdate +PlugClean +qall
+        fi
+    }
+
+    function update-nix-and-packages {
+        if command -v nix; then
+            sudo -i nix upgrade-nix
+            nix-channel --update
+
+            if command -v home-manager; then
+                home-manager switch
+            fi
+        fi
+    }
+
+    function update-snap-and-flatpak-packages {
+        if [ "$NICK_LINUX" = "true" ] && [ "$NICK_WSL2" != "true" ]; then
+            if command -v snap; then
+                sudo snap refresh
+            fi
+            if command -v flatpak; then
+                flatpak update -y
+                flatpak uninstall --unused
+                flatpak repair
+            fi
+        fi
+    }
+
+    function update-crates {
+        if command -v cargo; then
+            if [ ! -f $HOME/.cargo/bin/cargo-install-update ]; then
+                cargo install --locked cargo-update
+            fi
+            cargo install-update -a
+        fi
+    }
+
     set -x
 
-    # OS-specific package upgrades
-    if [ "$NICK_OS" = "ubuntu" ] || [ "$NICK_OS" = "pop" ]; then
-        sudo apt update
-        sudo apt upgrade -y
-        sudo apt autoremove -y
-    elif [ "$NICK_OS" = "fedora" ]; then
-        if command -v dnf5; then
-            sudo dnf5 upgrade -y --refresh
-            sudo dnf autoremove -y
-        else
-            sudo dnf upgrade -y --refresh
-            sudo dnf autoremove -y
-        fi
-    elif [ "$NICK_OS" = "opensuse-tumbleweed" ]; then
-        sudo zypper update -y
-    elif [ "$NICK_OS" = "darwin" ] && command -v brew; then
-        brew update
-        brew upgrade
-        brew cleanup
-    fi
-
-    # Toolchain and package updates
-    if command -v rustup; then
-        rustup update
-    fi
-    if [ -f "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim ] && command -v nvim; then
-        nvim --headless +PlugUpgrade +PlugUpdate +PlugClean +qall
-    fi
-
-    # Nix related updates
-    if command -v nix; then
-        sudo -i nix upgrade-nix
-        nix-channel --update
-
-        if command -v home-manager; then
-            home-manager switch
-        else
-            nix profile upgrade '.*'
-        fi
-    fi
-
-    # Linux desktop snap and flatpak upgrades
-    if [ "$NICK_LINUX" = "true" ] && [ "$NICK_WSL2" != "true" ]; then
-        if command -v snap; then
-            sudo snap refresh
-        fi
-        if command -v flatpak; then
-            flatpak update -y
-            flatpak uninstall --unused
-            flatpak repair
-        fi
-    fi
-
-    # Update crates
-    if command -v cargo; then
-        if [ ! -f $HOME/.cargo/bin/cargo-install-update ]; then
-            cargo install --locked cargo-update
-        fi
-        cargo install-update -a
-    fi
+    prepare-permissions
+    update-os-packages
+    update-rustup
+    update-neovim-plugins
+    update-nix-and-packages
+    update-snap-and-flatpak-packages
+    update-crates
 
     set +x
 }
