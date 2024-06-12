d5ce20356e494e389d55188b6c97ef81c478c2ac
Nick Gerace <nickagerace@gmail.com>
2024-06-12 01:02:50 -0400

    Refactor bootstrapping script to log individual lines
    This commit refactors the bootstrapping script to log individual lines
    rather than blocks and other segmentations of code. This is a first step
    in reducing the complexity of the script by allowing it to log
    with the same logging functions rather than with complex nesting and
    difficult to parse messages.
    
    Bootstrapper changes:
    - Make logging for bootstrap script consistent and go line by line (the
      UX is a work in progress, but this the intended direction)
    - Polish linking steps
    - Consolidate WSL2 checking logic
    - Disable cargo package install for darwin
    - Polish Rust install functions
    - Remove neovim plugin step
    - Split non-essential packages for darwin into a new file
    - Remove Git user name step
    
    Other changes:
    - Retire tmux and neovim configs (and remove tmux and neovim from darwin
      base packages)
    - Move neovim update plugins function from global update function
      to its own, independent function
    
    Signed-off-by: Nick Gerace <nickagerace@gmail.com>

--- zsh/nvim.zsh
+++ zsh/nvim.zsh
@@ -1,4 +1,10 @@
 if [ "$(command -v nvim)" ]; then
     alias nvim-upgrade="nvim +PlugUpgrade +PlugUpdate +PlugClean +qall"
     alias nvim-install="nvim +PlugInstall +qall"
+
+    function update-neovim-plugins {
+        if [ -f "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim ] && command -v nvim; then
+            nvim --headless +PlugUpgrade +PlugUpdate +PlugClean +qall
+        fi
+    }
 fi
