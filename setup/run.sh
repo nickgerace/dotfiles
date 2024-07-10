037c4f76cec3b00baec3c7bb84baf6a06c9e06f0
Nick Gerace <nickagerace@gmail.com>
2024-07-10 12:42:05 -0400

    Add arch linux to setup
    This commit adds arch linux to the setup script and to the dotfiles in
    general. In addition, the repository has switched back to alacritty
    from wezterm due to wayland support and only needing one multiplexer
    (zellij or tmux).
    
    Secondary changes:
    - Add system76 software installer for arch linux
    - Remove "pkgs" and "home-manager" directories in favor of
      distro-specific directories
    - Make "cargo/config.toml" specific to arch linux
    - Ensure helix works with arch linux since the binary is "helix" instead
      of "hx"
    - Use catppuccin latte where necessary
      - Alacritty: https://github.com/catppuccin/alacritty
      - Helix: https://github.com/catppuccin/helix
    - Replace or remove comments with log output in setup script
    
    Signed-off-by: Nick Gerace <nickagerace@gmail.com>

--- setup/run.sh
+++ setup/run.sh
@@ -41,13 +41,13 @@ while true; do
   esac
 done
 
-# Check for non-root user
+log "Checking user..."
 if [ $EUID -eq 0 ]; then
   log-error "must run as non-root user"
   exit 1
 fi
 
-# Check if we are on a bootstrap-compatible platform
+log "Checking platform..."
 if [ "$(uname -s)" = "Linux" ]; then
   if [ -f /proc/sys/kernel/osrelease ] && grep "WSL2" /proc/sys/kernel/osrelease; then
     log "WSL2 detected!"
@@ -56,11 +56,13 @@ if [ "$(uname -s)" = "Linux" ]; then
     if [[ "$ID" = "pop" ]]; then
       log "Found bootstrap-compatible platform: Pop!_OS x86_64"
       BOOTSTRAP_PLATFORM="pop"
+    elif [[ "$ID" = "arch" ]]; then
+      log "Found bootstrap-compatible platform: Arch Linux x86_64"
+      BOOTSTRAP_PLATFORM="arch"
     fi
   fi
 fi
 
-# Link all dotfiles
 function link {
   if [ ! -f "$1" ]; then
     log-error "File does not exist: $1"
@@ -80,50 +82,47 @@ function link {
 
   ln -s "$1" "$2"
 }
+
 log "Setting up dotfiles..."
+git config --global pull.rebase true
+
 link "$BOOTSTRAP_DOTFILES_DIRECTORY/zshrc" "$HOME/.zshrc"
-link "$BOOTSTRAP_DOTFILES_DIRECTORY/wezterm.lua" "$HOME/.wezterm.lua"
 link "$BOOTSTRAP_DOTFILES_DIRECTORY/starship.toml" "$HOME/.config/starship.toml"
-link "$BOOTSTRAP_DOTFILES_DIRECTORY/cargo-config-global.toml" "$HOME/.cargo/config.toml"
 link "$BOOTSTRAP_DOTFILES_DIRECTORY/helix/config.toml" "$HOME/.config/helix/config.toml"
 link "$BOOTSTRAP_DOTFILES_DIRECTORY/helix/languages.toml" "$HOME/.config/helix/languages.toml"
+link "$BOOTSTRAP_DOTFILES_DIRECTORY/helix/themes/catppuccin_latte.toml" "$HOME/.config/helix/catppuccin_latte.toml"
 link "$BOOTSTRAP_DOTFILES_DIRECTORY/zellij/config.kdl" "$HOME/.config/zellij/config.kdl"
+link "$BOOTSTRAP_DOTFILES_DIRECTORY/alacritty/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml"
+link "$BOOTSTRAP_DOTFILES_DIRECTORY/alacritty/catppuccin-latte.toml" "$HOME/.config/alacritty/catppuccin-latte.toml"
 
+# TODO(nick): rework gfold to not need multiple configs.
 if [ "$(uname -s)" = "Darwin" ]; then
   link "$BOOTSTRAP_DOTFILES_DIRECTORY/gfold/darwin.toml" "$HOME/.config/gfold.toml"
 elif [ "$(uname -s)" = "Linux" ]; then
   link "$BOOTSTRAP_DOTFILES_DIRECTORY/gfold/linux.toml" "$HOME/.config/gfold.toml"
 fi
 
-if [ "$BOOTSTRAP_PLATFORM" = "pop" ]; then
-  link "$BOOTSTRAP_DOTFILES_DIRECTORY/home-manager/pop/home.nix" "$HOME/.config/home-manager/home.nix"
+if [ "$BOOTSTRAP_PLATFORM" = "arch" ]; then
+  link "$BOOTSTRAP_DOTFILES_DIRECTORY/arch-linux/cargo/config.toml" "$HOME/.cargo/config.toml"
+elif [ "$BOOTSTRAP_PLATFORM" = "pop" ]; then
+  link "$BOOTSTRAP_DOTFILES_DIRECTORY/pop-os/home-manager/home.nix" "$HOME/.config/home-manager/home.nix"
 fi
 
-# Configure git
-git config --global pull.rebase true
-
-# Bail if we do not wish to install packages
 if [ "$BOOTSTRAP_INSTALL_PACKAGES" != "true" ]; then
   log-success "Success!"
   exit 0
 elif [ "$BOOTSTRAP_PLATFORM" = "false" ]; then
   log-error "dotfiles setup succeeded, but skipping bootstrapper"
-  log-error "available bootstrapper platform(s): Pop!_OS x86_64"
+  log-error "available bootstrapper platform(s): Pop!_OS x86_64, Arch Linux x86_64"
   exit 1
-fi
-
-# Bootstrap based on the platform discovered
-if [ "$BOOTSTRAP_PLATFORM" = "pop" ]; then
-  # Prepare package installation
+elif [ "$BOOTSTRAP_PLATFORM" = "pop" ]; then
   log "Setting up package installation..."
   sudo apt update
   sudo apt upgrade -y
 
-  # Install base packages
   log "Installing base packages..."
-  xargs sudo apt install -y < "$BOOTSTRAP_DOTFILES_DIRECTORY/pkgs/pop/base-packages.txt"
+  xargs sudo apt install -y < "$BOOTSTRAP_DOTFILES_DIRECTORY/pop-os/pkgs/core.lst"
 
-  # Install Iosevka Nerd Font
   if [ ! -f "$HOME/.local/share/fonts/IosevkaNerdFont-Regular.ttf" ]; then
     log "Installing iosevka nerd font..."
     mkdir -p "$HOME/.local/share/fonts"
@@ -137,7 +136,7 @@ if [ "$BOOTSTRAP_PLATFORM" = "pop" ]; then
     fc-cache
   fi
 
-  # Install nix
+  log "Checking for nix installation..."
   if ! command -v nix && [ -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]; then
     . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
   fi
@@ -146,7 +145,7 @@ if [ "$BOOTSTRAP_PLATFORM" = "pop" ]; then
     curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
   fi
 
-  # Add zsh to nix shells
+  log "Checking default shell..."
   if [ "$SHELL" != "$HOME/.nix-profile/bin/zsh" ]; then
     if [[ ! $(grep "$HOME/.nix-profile/bin/zsh" /etc/shells) ]]; then
       echo "$HOME/.nix-profile/bin/zsh" |sudo tee -a /etc/shells
@@ -155,7 +154,6 @@ if [ "$BOOTSTRAP_PLATFORM" = "pop" ]; then
     chsh -s "$HOME/.nix-profile/bin/zsh"
   fi
 
-  # Install rustup and rust
   if ! command -v rustup && [ -f "$HOME/.cargo/env" ]; then
     source "$HOME/.cargo/env"
   fi
@@ -165,11 +163,9 @@ if [ "$BOOTSTRAP_PLATFORM" = "pop" ]; then
     source "$HOME/.cargo/env"
   fi
   log "Setting up rust toolchain..."
-  rustup toolchain install stable-x86_64-unknown-linux-gnu
-  rustup default stable-x86_64-unknown-linux-gnu
+  rustup default stable
   rustup component add rust-analyzer
 
-  # Install home-manager
   if ! command -v home-manager; then
     log "Installing home-manager..."
     nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
@@ -179,7 +175,6 @@ if [ "$BOOTSTRAP_PLATFORM" = "pop" ]; then
   log "Setting up home-manager..."
   home-manager switch
 
-  # Install docker
   if ! command -v docker; then
     log "Installing docker..."
     sudo install -m 0755 -d /etc/apt/keyrings
@@ -195,8 +190,7 @@ if [ "$BOOTSTRAP_PLATFORM" = "pop" ]; then
     sudo docker run hello-world
   fi
 
-  # Install latest node lts and npm packages (fnm comes from home-manager)
-  log "Setting up fnm..."
+  log "Setting up fnm (comes with home-manager)..."
   eval "$(fnm env --use-on-cd)"
   log "Installing latest node lts via fnm..."
   fnm install --lts
@@ -207,7 +201,6 @@ if [ "$BOOTSTRAP_PLATFORM" = "pop" ]; then
    typescript \
    typescript-language-server
 
-  # Install flatpaks
   log "Installing flatpaks..."
   flatpak install flathub \
     com.discordapp.Discord \
@@ -215,10 +208,8 @@ if [ "$BOOTSTRAP_PLATFORM" = "pop" ]; then
     com.slack.Slack \
     com.spotify.Client \
     md.obsidian.Obsidian \
-    org.wezfurlong.wezterm \
     us.zoom.Zoom
 
-  # Last step
   log "Running final update and cleanup commands..."
   rustup update
   sudo -i nix upgrade-nix
@@ -231,5 +222,67 @@ if [ "$BOOTSTRAP_PLATFORM" = "pop" ]; then
   sudo apt upgrade -y
   sudo apt autoremove -y
   sudo apt clean -y
+  log-success "Success!"
+elif [ "$BOOTSTRAP_PLATFORM" = "arch" ]; then
+  log "Setting up package installation..."
+  sudo pacman -Syu --noconfirm
+
+  log "Installing core base packages..."
+  sudo pacman -S --noconfirm --needed - < "$BOOTSTRAP_DOTFILES_DIRECTORY/arch-linux/pkgs/core.lst"
+
+  log "Installing AUR base packages..."
+  paru -S --noconfirm --needed - < "$BOOTSTRAP_DOTFILES_DIRECTORY/arch-linux/pkgs/aur.lst"
+
+  log "Checking for nix installation..."
+  if ! command -v nix && [ -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]; then
+    . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+  fi
+  if ! command -v nix; then
+    log "Installing nix..."
+    curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
+  fi
+
+  log "Checking default shell..."
+  if [ "$SHELL" != "$(which zsh)" ]; then
+    log "Changing default shell to zsh..."
+    chsh -s "$(which zsh)"
+  fi
+
+  log "Setting up rust via rustup..."
+  rustup default stable
+  rustup component add rust-analyzer
+
+  log "Setting up docker..."
+  sudo systemctl enable --now docker.socket
+  sudo usermod -aG docker "$USER"
+  sudo docker run hello-world
+
+  log "Setting up fnm..."
+  eval "$(fnm env --use-on-cd)"
+  log "Installing latest node lts via fnm..."
+  fnm install --lts
+  log "Installing npm packages..."
+  npm i -g \
+   @vue/language-server \
+   prettier \
+   typescript \
+   typescript-language-server
+
+  log "Installing flatpaks..."
+  flatpak install -y flathub \
+    com.discordapp.Discord \
+    com.google.Chrome \
+    com.slack.Slack \
+    com.spotify.Client \
+    md.obsidian.Obsidian \
+    us.zoom.Zoom
+
+  log "Running final update and cleanup commands..."
+  sudo -i nix upgrade-nix
+  nix-channel --update
+  flatpak update -y
+  flatpak uninstall --unused
+  flatpak repair
+  sudo pacman -Syu --noconfirm
   log-success "Success!"
 fi
