1093f3ccb212f993d83feaca740574b92258140b
Nick Gerace <nickagerace@gmail.com>
2024-07-13 18:21:23 -0400

    Add zellij edit alias
    Signed-off-by: Nick Gerace <nickagerace@gmail.com>

--- zsh/zellij.zsh
+++ zsh/zellij.zsh
@@ -3,4 +3,5 @@ if [ "$(command -v zellij)" ]; then
     alias zh="zellij --help"
     alias za="zellij a"
     alias zdaf="zellij delete-all-sessions -y"
+    alias hxz="hx ~/.config/zellij/config.kdl"
 fi
