6dfdd35377ae43f7e2c0576f65c0c5b08a483e79
Nick Gerace <nickagerace@gmail.com>
2024-09-02 21:36:21 -0400

    Add hostname to prompt and adjust colors
    Signed-off-by: Nick Gerace <nickagerace@gmail.com>

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,14 +1,10 @@
-# NOTE(nick): this prompt uses catppuccin latte colors:
-# - directory: sapphire
-# - branch: pink
-# - success prompt: green
-# - fail prompt: red
+# This uses catppuccin latte colors: https://github.com/catppuccin/catppuccin
 setopt PROMPT_SUBST
 autoload -Uz vcs_info
 zstyle ':vcs_info:*' enable git
-zstyle ':vcs_info:git*' formats 'on %B%F{#ea76cb}%b%f'
+zstyle ':vcs_info:git*' formats ' on %B%F{#40a02b}%b%f'
 precmd() { vcs_info }
 
-# NOTE(nick): the PROMPT must use single quotes in order to properly work, which includes updating "vcs_info".
-PROMPT='%B%F{#209fb5}%~%f%b ${vcs_info_msg_0_}
+# The PROMPT must use single quotes in order to properly work, which includes updating "vcs_info".
+PROMPT='%B%F{#209fb5}%~%f%b${vcs_info_msg_0_} %b(%B%F{#8839ef}%m%f%b)
 %(?.%B%F{#40a02b}❯%f%b.%B%F{#d20f39}❯%f%b) '
