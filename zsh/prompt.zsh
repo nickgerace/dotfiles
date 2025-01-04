447c5d74e7c41fefd47796debc24b8b9e506ae9e
Nick Gerace <nickagerace@gmail.com>
2025-01-04 14:23:56 -0500

    Replace hardcoded prompt colors
    Signed-off-by: Nick Gerace <nickagerace@gmail.com>

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,24 +1,9 @@
-# This uses catppuccin latte colors: https://github.com/catppuccin/catppuccin
 setopt PROMPT_SUBST
 autoload -Uz vcs_info
 zstyle ':vcs_info:*' enable git
-zstyle ':vcs_info:git*' formats ' on %B%F{#40a02b}%b%f'
+zstyle ':vcs_info:git*' formats ' on %B%F{green}%b%f'
 precmd() { vcs_info }
 
-# Colors for catppuccin-latte (light)
-# Source: https://catppuccin.com/palette
-# SAPPHIRE="#209fb5"
-# MAUVE="#8839ef"
-# GREEN="#40a02b"
-# RED="#d20f39"
-
-# Colors for catppuccin-frappe (dark)
-# Source: https://catppuccin.com/palette
-SAPPHIRE="#85c1dc"
-MAUVE="#ca9ee6"
-GREEN="#a6d189"
-RED="#e78284"
-
-# The PROMPT must use single quotes in order to properly work, which includes updating "vcs_info".
-PROMPT='%B%F{$SAPPHIRE}%~%f%b${vcs_info_msg_0_} %b(%B%F{$MAUVE}%m%f%b)
-%(?.%B%F{$GREEN}❯%f%b.%B%F{$RED}❯%f%b) '
+# The PROMPT must use single quotes in order to properly work, which includes updating "vcs_info"
+PROMPT='%B%F{blue}%~%f%b${vcs_info_msg_0_} %b(%B%F{magenta}%m%f%b)
+%(?.%B%F{green}❯%f%b.%B%F{red}❯%f%b) '
