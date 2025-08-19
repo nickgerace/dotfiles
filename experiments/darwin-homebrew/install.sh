3e15b98eb41aa4b4c257b6075c9d09aaf219cd57
Nick Gerace <nickagerace@gmail.com>
2025-08-18 23:24:23 -0400

    Add darwin homebrew experiment and fix fnm for zsh
    Signed-off-by: Nick Gerace <nickagerace@gmail.com>

---
+++ experiments/darwin-homebrew/install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+xargs brew install < core.lst
+
+# INSTALL CALIGULA FOR BURNING ISOs
+# brew tap philocalyst/tap
+# brew install caligula
--- zsh/fnm.zsh
+++ zsh/fnm.zsh
@@ -1,3 +1,3 @@
 if [ "$(command -v fnm)" ]; then
-  eval "$(fnm env --use-on-cd)"
+  eval "$(fnm env --use-on-cd --shell zsh)"
 fi
