004a8d922bd946870c8882bc3142d12f9da5503b
Nick Gerace <nickagerace@gmail.com>
2026-03-02 13:49:57 -0500

    Remove hostname from prompt
    Signed-off-by: Nick Gerace <nickagerace@gmail.com>

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -5,5 +5,5 @@ zstyle ':vcs_info:git*' formats ' on %B%F{green}%b%f'
 precmd() { vcs_info }
 
 # The PROMPT must use single quotes in order to properly work, which includes updating "vcs_info"
-PROMPT='%B%F{blue}%~%f%b${vcs_info_msg_0_} %b(%B%F{magenta}%m%f%b)
+PROMPT='%B%F{blue}%~%f%b${vcs_info_msg_0_}
 %(?.%B%F{green}❯%f%b.%B%F{red}❯%f%b) '
